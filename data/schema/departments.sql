9db34fab3c07a784503d488e561852f263b68125
Marcus Sacramento <marcus.vinicius.cardozo@gmail.com>
2016-07-09 12:09:57 -0300

    Alterando a tabela

--- data/schema/departments.sql
+++ data/schema/departments.sql
@@ -2,6 +2,7 @@ CREATE TABLE `departments` (
   `dept_no` char(4) NOT NULL,
   `dept_name` varchar(40) NOT NULL,
   `teste` int(11) NOT NULL,
+  `nova_coluna` int(11) NOT NULL,
   PRIMARY KEY (`dept_no`),
   UNIQUE KEY `dept_name` (`dept_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1
